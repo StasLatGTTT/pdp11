3bca2834585ef3c4807c674e26e627268739a918
Stanislau Latushko <staslatgttt@gmail.com>
2017-01-18 18:02:42 +0000

    added example into instr_implementation.h

--- src/emul/instr_implementation.h
+++ src/emul/instr_implementation.h
@@ -1,4 +1,12 @@
 /*
-	Implementation of _fetch, _work and _writeback of
+	Implementation of _fetch, _execute and _writeback of
 	every possible instruction
 */
+
+#include "interstate.h"
+#include "proc.h"
+
+//add instruction implementation
+int add_fetch(interstate*, proc*);
+int add_execute(interstate*);
+int add_writeback(interstate*, proc*);
