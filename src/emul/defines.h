bc90da16998782750610a7503a757d2892e78160
Stanislau Latushko <staslatgttt@gmail.com>
2017-01-18 15:58:05 +0000

    added some headers and added defines for registers

---
+++ src/emul/defines.h
@@ -0,0 +1,15 @@
+/*
+	Store defines to make work with emulator easier
+*/
+
+#define R0 0x00
+#define R1 0x01
+#define R2 0x02
+#define R3 0x03
+#define R4 0x04
+#define R5 0x05
+#define R7 0x06
+#define R8 0x07
+
+#define SP 0x06
+#define PC 0x07
