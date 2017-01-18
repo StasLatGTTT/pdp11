1c9ad9a3b42f68e53c87e614c613f8fd7789e2ca
Stanislau Latushko <staslatgttt@gmail.com>
2017-01-18 17:42:39 +0000

    filled interstate.h

--- src/emul/interstate.h
+++ src/emul/interstate.h
@@ -1,4 +1,13 @@
 /*
 	Describe interstate of instruction execution between fetch, work and
 	writeback
+	Asignee: Yaroslav
 */
+
+struct interstate{
+	uint8_t opcode;
+	uint8_t mode1;
+	uint8_t src;
+	uint8_t mode2;
+	uint8_t dst;
+}
