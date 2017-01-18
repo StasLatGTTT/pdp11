cd7668e0df899adf3201d30824e7aa9267a1b3f4
Stanislau Latushko <staslatgttt@gmail.com>
2017-01-18 17:39:50 +0000

    added proc class declaration

--- src/emul/proc.h
+++ src/emul/proc.h
@@ -1,3 +1,21 @@
 /*
 	Class to emulate CPU
 */
+
+#include <cstdint>
+
+#include "interstate.h"
+
+class proc{
+	//64-bit
+	int8_t* memory;
+
+	//32-bit
+	int32_t ticks_seqential, ticks_pipelined;
+
+	//16-bit
+	int16_t[8] registers;
+
+	//8-bit
+	int8_t statword;
+}
