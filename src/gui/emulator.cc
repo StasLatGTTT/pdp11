42a907bb071c9c2b0818d505877353597e246f9e
EnjeruGorkaya <tanya.meow13@gmail.com>
2017-01-20 18:08:54 +0300

    add simple gui

--- src/gui/emulator.cc
+++ src/gui/emulator.cc
@@ -1,5 +1,7 @@
 #include "emulator.h"
 
+#include "../../ui_testwidget.h"
+
 Emulator_App::Emulator_App(int argc, char** argv, Proc* cpu){
 	this->cpu = cpu;
 	this->argc = argc;
@@ -13,8 +15,14 @@ Emulator_App::~Emulator_App(){
 int Emulator_App::run(){
 	emul_app = new QApplication(argc, argv);
 
-	QTextEdit tedit;
-	tedit.show();
+	//QTextEdit tedit;
+	//tedit.show();
+
+	QWidget *widget = new QWidget;
+	Ui::TestWidget ui;
+	ui.setupUi(widget);
+
+	widget->show();
 
 	return emul_app->exec();
 }
